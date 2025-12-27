6be9eb97180f8d6c2d5a087daf0f221db8551f14
nihad1213 <nihad.nemetli@gmail.com>
2025-12-27 13:32:37 +0400

    upd: header part fixed

--- backend/App/Controller/AbstractController.php
+++ backend/App/Controller/AbstractController.php
@@ -9,7 +9,7 @@ abstract class AbstractController
     protected function jsonResponse(array $data, int $statusCode = 200): void
     {
         http_response_code($statusCode);
-        header('Content-Type: application/json');
+        header('Content-Type: application/json; charset=utf-8');
         echo json_encode($data);
     }
 
